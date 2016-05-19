35ae41ff61bbb0ec64a4f6868d556eb5cca55980
ndmanvar <ndmanvar@yahoo.com>
2016-05-19 16:52:40 -0700

    Fixing exec statement

--- code_coverage_updater.rb
+++ code_coverage_updater.rb
@@ -2,11 +2,11 @@ if `git log -n 1`.include? 'Update coverage'
 	puts 'do not need to update coverage number'
 else
 	puts 'Updating coverage number...'
-	exec(printenv)
-	exec(git checkout master)
-	exec(git pull origin master)
-	exec(make all)
-	exec(git config --global user.email "ndmanvar_prbuild@yahoo.com")
-	exec(git config --global user.name "ndmanvar-prbuild")
-	exec(git add coverage && git commit -m 'Update coverage number' && git push)
+	exec("printenv")
+	exec("git checkout master")
+	exec("git pull origin master")
+	exec("make all")
+	exec("git config --global user.email \"ndmanvar_prbuild@yahoo.com\"")
+	exec("git config --global user.name \"ndmanvar-prbuild\"")
+	exec("git add coverage && git commit -m 'Update coverage number' && git push")
 end
