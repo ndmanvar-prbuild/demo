9462c3ae662487d7fdcbdad4ee477f1a146154ae
ndmanvar <ndmanvar@yahoo.com>
2016-05-19 16:51:20 -0700

    Using exec

--- code_coverage_updater.rb
+++ code_coverage_updater.rb
@@ -2,11 +2,11 @@ if `git log -n 1`.include? 'Update coverage'
 	puts 'do not need to update coverage number'
 else
 	puts 'Updating coverage number...'
-	`printenv`
-	`git checkout master`
-	`git pull origin master`
-	`make all`
-	`git config --global user.email "ndmanvar_prbuild@yahoo.com"`
-	`git config --global user.name "ndmanvar-prbuild"`
-	`git add coverage && git commit -m 'Update coverage number' && git push`
+	exec(printenv)
+	exec(git checkout master)
+	exec(git pull origin master)
+	exec(make all)
+	exec(git config --global user.email "ndmanvar_prbuild@yahoo.com")
+	exec(git config --global user.name "ndmanvar-prbuild")
+	exec(git add coverage && git commit -m 'Update coverage number' && git push)
 end
