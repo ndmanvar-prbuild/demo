9aa2ad41b49db8ee2987df4b08f916ec9e74d1bc
ndmanvar <ndmanvar@yahoo.com>
2016-05-19 17:13:16 -0700

    adding code_cov update.sh

---
+++ code_coverage_update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+recentCommit=`git log -n 1`
+
+if [[ "$recentCommit" =~ "Update coverage" ]]; then
+    echo "do not need to run through tests, only code coverage num was updated"
+else
+	git checkout master
+	git pull origin master
+	make all
+	git config --global user.email "ndmanvar_prbuild@yahoo.com"
+	git config --global user.name "ndmanvar-prbuild"
+	git add coverage && git commit -m 'Update coverage number' && git push
+fi
