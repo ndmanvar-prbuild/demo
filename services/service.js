439087f1b620fd434a95d8bb627f6575b57f8f13
ndmanvar <ndmanvar@yahoo.com>
2016-06-02 10:24:44 -0500

    Change in src code 71 to 81

--- services/service.js
+++ services/service.js
@@ -13,10 +13,6 @@ app.service('service', function () {
 
 });
 
-var data = [{'data1': 71, 'data2': 72}];
-
-if (false) {
-	console.log('this is garbage');
-}
+var data = [{'data1': 81, 'data2': 82}];
 
 
