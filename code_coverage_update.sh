75e15fd16f839c10eb122444320e96019897cfa0
ndmanvar <ndmanvar@yahoo.com>
2016-07-13 11:05:39 -0400

    commenting back in

--- code_coverage_update.sh
+++ code_coverage_update.sh
@@ -6,11 +6,11 @@ if [[ "$recentCommit" =~ "Update coverage" ]]; then
     echo "do not need to run through tests, only code coverage num was updated"
 else
 	echo "running through tests and updating coverage numbers"
-	# git checkout master
-	# git pull origin master
-	# make all
-	# git config --global user.email "ndmanvar_prbuild@yahoo.com"
-	# git config --global user.name "ndmanvar-prbuild"
-	# (git add coverage && git commit -m 'Update coverage number' && git push) || echo 'Nothing to push!'
-	# (git checkout prod && git pull origin master && git push origin prod) || echo "updated prod"
+	git checkout master
+	git pull origin master
+	make all
+	git config --global user.email "ndmanvar_prbuild@yahoo.com"
+	git config --global user.name "ndmanvar-prbuild"
+	(git add coverage && git commit -m 'Update coverage number' && git push) || echo 'Nothing to push!'
+	(git checkout prod && git pull origin master && git push origin prod) || echo "updated prod"
 fi
