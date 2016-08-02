c002bf08ac9f688b143cc44bd21c58db361ac410
ndmanvar <ndmanvar@yahoo.com>
2016-08-01 20:49:34 -0700

    using docker for deploy and test

--- deploy_and_test.sh
+++ deploy_and_test.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
 # deploy and keep track of pid
-echo "Deploying via SimpleHTTPServer..."
-python -m SimpleHTTPServer &
-FOO_PID=$!
-sleep 3
+echo "Deploying via Docker..."
+
+NAME="docker_name"
+# builds
+docker rm $NAME || echo "No name by image exists! Moving on..."
+docker build -t demo_test .
+
+docker run -p 8000:8000 --name $NAME demo_test &
+
+sleep 15
 
 # run tests
 echo "Running Functional Tests using Protractor"
 if ./node_modules/.bin/protractor conf.js ; then
 	# shut down server
-	kill $FOO_PID
+	docker stop $NAME
 	# return appropriate exit code
 	exit 0
 else
 	# shut down server
-	kill $FOO_PID
+	docker stop $NAME
 	# return appropriate exit code
 	exit 1
 fi
+
+
+# need to stop
