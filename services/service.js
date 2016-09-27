9005966265faa3694f25108a2f01eedc09c447fb
ndmanvar <ndmanvar@yahoo.com>
2016-09-27 11:25:57 -0700

    fix src code

--- services/service.js
+++ services/service.js
@@ -11,9 +11,5 @@ app.service('service', function () {
     };
 });
 
-var data = [{'data1': 21, 'data2': 22}];
-
-if (false) {
-	console.log('this is garbage');
-}
+var data = [{'data1': 31, 'data2': 32}];
 
