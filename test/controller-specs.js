04c0f09ad57b594de0af6737e2ca2f0f53df8ad7
ndmanvar <ndmanvar@yahoo.com>
2016-09-28 17:07:43 -0700

    adding tests

--- test/controller-specs.js
+++ test/controller-specs.js
@@ -24,8 +24,15 @@ describe('ControlerTest', function() {
       expect($scope.data[1].data1).toEqual(8);
     });
 
+    it('should remove data', function() {
+      var $scope = {};
+      var controller = $controller('controller', { $scope: $scope });
+      $scope.removeData();
+      expect($scope.data).toEqual([ {data1: 11, data2: 12}]);
+    });
+
     it('should reset newData after the function', function() {
-      var $scope = {newData: {data1: 8, data2: 9}};
+      var $scope = {newData: {data1: 18, data2: 19}};
       var controller = $controller('controller', { $scope: $scope });
       $scope.insertData();
       expect($scope.newData.data1).toEqual('');
