01329c2bed691581ce975e4ddce66aa8a8017bc6
ndmanvar <ndmanvar@yahoo.com>
2016-11-04 08:44:28 -0700

    41 and 42

--- services/service.js
+++ services/service.js
@@ -12,8 +12,5 @@ app.service('service', function () {
 
 });
 
-var data = [{'data1': 31, 'data2': 32}];
+var data = [{'data1': 41, 'data2': 42}];
 
-if (false) {
-	console.log('this is garbage');
-}
