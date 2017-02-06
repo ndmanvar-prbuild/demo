6927e67fd16a2c817b690fb4f0dff3f7e39a019c
ndmanvar <ndmanvar@yahoo.com>
2017-02-07 10:02:17 +1100

    resetting

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -15,9 +15,5 @@ app.controller('controller', function ($scope, service) {
         $scope.newData.data2 = '';
     };
 
-    $scope.removeData = function () {
-        service.removeLastData();
-    };
-
     $scope.data = data;
 });
